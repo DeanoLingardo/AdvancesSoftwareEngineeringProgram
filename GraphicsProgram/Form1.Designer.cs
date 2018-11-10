050b06e024de4c543af14f7df1da2b3402017082
Dean Lingard <Dean Linhgard@DESKTOP-JMAVLGI>
2018-11-10 15:19:52 +0000

    Text input added, button fuctions now work in command line

--- GraphicsProgram/Form1.Designer.cs
+++ GraphicsProgram/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             Ellipse = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -52,7 +53,7 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(256, 42);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1456, 762);
+            this.pictureBox1.Size = new System.Drawing.Size(1456, 696);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
@@ -78,13 +79,21 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(32, 753);
+            this.button3.Location = new System.Drawing.Point(32, 768);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(182, 51);
+            this.button3.Size = new System.Drawing.Size(182, 36);
             this.button3.TabIndex = 6;
             this.button3.Text = "Clear";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(599, 773);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(784, 31);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // InitialTestForm
             // 
@@ -92,6 +101,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Coral;
             this.ClientSize = new System.Drawing.Size(1741, 834);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -102,6 +112,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,6 +122,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- GraphicsProgram/Form1.cs
+++ GraphicsProgram/Form1.cs
@@ -26,17 +26,12 @@ namespace GraphicsProgram
  
         private void button3_Click(object sender, EventArgs e)
         {
-
             g.DrawEllipse(new Pen(Color.Black), 100, 100, 250, 250);
-          
-
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-
             g.DrawRectangle(new Pen(Color.Black), 400, 100, 250, 250);
-
         }
 
         private void button2_Click_1(object sender, EventArgs e)
@@ -49,5 +44,27 @@ namespace GraphicsProgram
             g.Clear(Color.Coral);
         }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string input;
+           input = textBox1.Text;
+
+            if (input == "rectangle")
+            {
+                g.DrawRectangle(new Pen(Color.Black), 200, 100, 450, 250);
+            }
+            if (input == "ellipse")
+            {
+                g.DrawEllipse(new Pen(Color.Black), 100, 100, 250, 250);
+            }
+            if (input == "line")
+            {
+                g.DrawLine(new Pen(Color.Black), 100, 100, 500, 500);
+            }
+            if (input == "clear")
+            {
+                g.Clear(Color.Coral);
+            }
+        }
     }
 }
