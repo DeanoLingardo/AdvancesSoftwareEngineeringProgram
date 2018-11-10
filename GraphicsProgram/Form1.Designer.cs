8a2f628ddfeae1a0e296c6a20caddf90472ea01c
Dean Lingard <Dean Linhgard@DESKTOP-JMAVLGI>
2018-11-10 15:01:06 +0000

    Shape drawing(Hardcoded x, y), Clear button & Picture box added

--- GraphicsProgram/Form1.Designer.cs
+++ GraphicsProgram/Form1.Designer.cs
@@ -28,61 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.EnterButton = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            System.Windows.Forms.Button Ellipse;
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            Ellipse = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // EnterButton
+            // Ellipse
             // 
-            this.EnterButton.Location = new System.Drawing.Point(812, 588);
-            this.EnterButton.Name = "EnterButton";
-            this.EnterButton.Size = new System.Drawing.Size(343, 55);
-            this.EnterButton.TabIndex = 0;
-            this.EnterButton.Text = "Enter";
-            this.EnterButton.UseVisualStyleBackColor = true;
-            this.EnterButton.Click += new System.EventHandler(this.button1_Click);
+            Ellipse.AllowDrop = true;
+            Ellipse.Location = new System.Drawing.Point(32, 42);
+            Ellipse.Name = "Ellipse";
+            Ellipse.Size = new System.Drawing.Size(167, 70);
+            Ellipse.TabIndex = 2;
+            Ellipse.Text = "Ellipse";
+            Ellipse.UseVisualStyleBackColor = true;
+            Ellipse.Click += new System.EventHandler(this.button3_Click);
             // 
-            // textBox1
+            // pictureBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(43, 600);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(748, 31);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.pictureBox1.Location = new System.Drawing.Point(256, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1456, 762);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
             // 
-            // groupBox1
+            // button1
             // 
-            this.groupBox1.Location = new System.Drawing.Point(43, 53);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1112, 511);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Output";
-            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            this.button1.Location = new System.Drawing.Point(32, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 67);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Rectangle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(32, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 68);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Line";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(32, 753);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 51);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
             // 
             // InitialTestForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkGray;
-            this.ClientSize = new System.Drawing.Size(1220, 689);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.EnterButton);
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(1741, 834);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(Ellipse);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.Name = "InitialTestForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
-        #endregion
 
-        private System.Windows.Forms.Button EnterButton;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.GroupBox groupBox1;
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- GraphicsProgram/Form1.cs
+++ GraphicsProgram/Form1.cs
@@ -12,29 +12,42 @@ namespace GraphicsProgram
 {
     public partial class InitialTestForm : Form
     {
+        private Graphics g;
+        private Pen myPen = new Pen(Color.Blue, 2);
+
+
         public InitialTestForm()
         {
             InitializeComponent();
+            this.Width = 1000;
+            this.Height = 500;
+            g = pictureBox1.CreateGraphics();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+ 
+        private void button3_Click(object sender, EventArgs e)
         {
 
+            g.DrawEllipse(new Pen(Color.Black), 100, 100, 250, 250);
+          
+
         }
 
-        private void groupBox1_Enter(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
 
+            g.DrawRectangle(new Pen(Color.Black), 400, 100, 250, 250);
+
         }
 
-        private void toolStripButton1_Click(object sender, EventArgs e)
+        private void button2_Click_1(object sender, EventArgs e)
         {
-
+            g.DrawLine(new Pen(Color.Black), 100, 100, 500, 500);
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button3_Click_1(object sender, EventArgs e)
         {
-
+            g.Clear(Color.Coral);
         }
+
     }
 }
